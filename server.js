715aaaff3a2dd1ee81fd4551d848df8be9ed4ae2
Lucre <josielindamaldonado@gmail.com>
2021-05-06 21:25:05 -0400

    trying to seed.

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 3000;
 
-const Excercises = require("./models/Excercises");
+const Workout = require("models/Workout.js");
 const app = express();
 
 // app.use(logger("dev"));
@@ -14,7 +14,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/userdb", { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
